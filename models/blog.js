6c73a7f4da32aad2919cf25b8f213a4454b005dd
yilin xu <xu.yilin@husky.neu.edu>
2020-07-22 23:19:06 -0400

    add validation of schema

--- models/blog.js
+++ models/blog.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+  title: { type: String, required: true, minlength: 3 },
+  author: { type: String, required: true, minlength: 3 },
+  url: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+});
 
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    },
-  })
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model("Blog", blogSchema);
