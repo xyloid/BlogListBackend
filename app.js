07a7aff06991341adc6dc1c628b74592d18e74c4
yilin xu <xu.yilin@husky.neu.edu>
2020-07-22 23:37:19 -0400

    ex4.1/4.2 update middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -20,7 +21,11 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs',blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
