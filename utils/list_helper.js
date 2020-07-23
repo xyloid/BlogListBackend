dcd1337ecd049baeb658c8865429028695fc83bb
yilin xu <xu.yilin@husky.neu.edu>
2020-07-23 12:48:04 -0400

    ex4.5 update

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length > 0) {
+    const result = blogs.reduce((prev, current) => {
+      return prev.likes > current.likes ? prev : current;
+    }, blogs[0]);
+    return {
+      title: result.title,
+      author: result.author,
+      likes: result.likes,
+    };
+  }
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
