f88402e898d18fa137f9b74db711744622de971f
yilin xu <xu.yilin@husky.neu.edu>
2020-07-24 22:57:41 -0400

    working on ex4.8

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,13 +2,25 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const blogHelper = require('./blog_helper')
+const Blog = require('../models/blog')
 
+beforeEach(async ()=>{
+    await Blog.deleteMany({})
+
+    await blogHelper.prepareBlogs()
+})
 
 test('blog list are returned as json', async ()=>{
     await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('number of blogs return should be correct', async ()=>{
+    let blogs = await api.get('/api/blogs')
+    expect(blogs.body).toHaveLength(blogHelper.initialBlogs.length)
 })
 
 
