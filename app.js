9cdb04d0b177595d256ab22d018e0bd40197be18
yilin xu <xu.yilin@husky.neu.edu>
2020-07-27 12:37:48 -0400

    can create new user with http post

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -25,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs',blogsRouter)
+app.use('/api/users',usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
