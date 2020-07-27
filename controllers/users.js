51386b8cc4b920b5f5fd68515a18ba8902d0fa1f
yilin xu <xu.yilin@husky.neu.edu>
2020-07-27 12:48:42 -0400

    ex4.16 update

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,10 @@ const User = require("../models/user");
 usersRouter.post("/", async (request, response) => {
   const body = request.body;
 
+  if(body.password.length<3){
+    return response.status(400).json({error:'password length should be more than 3 characters.'})
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ mongoose.set("useCreateIndex", true);
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-  username: { type: String, unique: true, required: true },
+  username: { type: String, unique: true, required: true, minlength:3 },
   name: String,
   passwordHash: String,
   blogs: [
