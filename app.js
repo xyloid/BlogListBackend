110b1fdee6d81f4c198fedf70cebc3cde129f4fa
yilin xu <xu.yilin@husky.neu.edu>
2020-08-01 23:27:13 -0400

    update for end to end test

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
-const Blog = require('./models/blog')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const express = require('express')
-const app = express()
-require('express-async-errors')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const Blog = require("./models/blog");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const express = require("express");
+const app = express();
+require("express-async-errors");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+    logger.error("error connection to MongoDB:", error.message);
+  });
 
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
+app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
-app.use('/api/blogs',blogsRouter)
-app.use('/api/users',usersRouter)
-app.use('/api/login',loginRouter)
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
 
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-module.exports = app
+module.exports = app;
